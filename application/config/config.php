b0cb82400b909a24dc4954619d70bb6fb9c1aef0
Yoga S Devanada <ydevanada@gmail.com>
2019-08-23 14:46:35 +0700

    Changes on base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://AutoKKN-2131913027.us-east-1.elb.amazonaws.com/kknumm/';
+$config['base_url'] = 'http://3.208.97.228/kknumm/';
 
 /*
 |--------------------------------------------------------------------------
