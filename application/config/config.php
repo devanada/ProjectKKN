9963cb828bb31b19c0de276257475f63a21c0963
Yoga S Devanada <ydevanada@gmail.com>
2019-08-23 15:02:56 +0700

    changed on config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://3.208.97.228/kknumm/';
+$config['base_url'] = 'http://3.208.97.228/';
 
 /*
 |--------------------------------------------------------------------------
